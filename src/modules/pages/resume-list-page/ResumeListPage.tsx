79973644cb29e81fee3200e5d72c7782f9852c7e
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-10 19:38:40 +0100

    create react routing

---
+++ src/modules/pages/resume-list-page/ResumeListPage.tsx
@@ -0,0 +1,5 @@
+export function ResumeListPage(){
+    return (
+        <div>test</div>
+    )
+}
---
+++ src/modules/pages/templeate-list-page/TemplateListPage.tsx
@@ -0,0 +1,5 @@
+export function  TemplateListPage(){
+    return (
+        <div>test</div>
+    )
+}
