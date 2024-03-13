835223255baff9b729ada5772ca8e8fd9befd9ff
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-13 18:29:22 +0100

    add header

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from './components/header/Header';
+import { Box } from '@chakra-ui/react';
 
 export function Layout() {
-  return <Outlet />;
+  return (
+    <Box h={'100vh'} bg={'gray.100'}>
+      <Header />
+      <Box h={'calc(100% - 87px)'} overflowY={'auto'}>
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
