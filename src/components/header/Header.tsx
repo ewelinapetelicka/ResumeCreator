d7f7d31249a96c4bad95c83cb24c06cae946661d
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-15 22:53:30 +0100

    add navigation on header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,35 +1,54 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
-    <Box
-      bg={'white'}
-      h={'87px'}
-      m={'0 10px 0 10px'}
-      p={'0 0 0 20px'}
-      gap={'5px'}
-      display={'flex'}
-      alignItems={'center'}
-      boxShadow={'0px 1px 4px #e1e1e1'}
-      borderRadius={'10px'}>
-      <Text fontWeight={'bolder'} fontSize={'xx-large'}>
-        R
-      </Text>
-      <Text
-        textTransform={'uppercase'}
-        fontWeight={'bolder'}
-        letterSpacing={'5px'}>
-        esume
-      </Text>
-      <Text fontWeight={'bolder'} fontSize={'xx-large'}>
-        C
-      </Text>
-      <Text
-        textTransform={'uppercase'}
-        fontWeight={'bolder'}
-        letterSpacing={'5px'}>
-        reator
-      </Text>
+    <Box>
+      <Box
+        bg={'white'}
+        h={'87px'}
+        m={'0 10px 0 10px'}
+        p={'0 0 0 20px'}
+        gap={'5px'}
+        display={'flex'}
+        alignItems={'center'}
+        boxShadow={'0px 1px 4px #e1e1e1'}
+        borderRadius={'10px'}>
+        <Text fontWeight={'bolder'} fontSize={'xx-large'}>
+          R
+        </Text>
+        <Text
+          textTransform={'uppercase'}
+          fontWeight={'bolder'}
+          letterSpacing={'5px'}>
+          esume
+        </Text>
+        <Text fontWeight={'bolder'} fontSize={'xx-large'}>
+          C
+        </Text>
+        <Text
+          textTransform={'uppercase'}
+          fontWeight={'bolder'}
+          letterSpacing={'5px'}>
+          reator
+        </Text>
+        <Box>
+          <Button
+            colorScheme="gray"
+            variant="ghost"
+            onClick={() => navigate('/resumes')}>
+            Resumes
+          </Button>
+          <Button
+            colorScheme="gray"
+            variant="ghost"
+            onClick={() => navigate('/templates')}>
+            Templates
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 }
