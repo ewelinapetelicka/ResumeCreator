3f0e00fde9662ff007ab564a02065a07318486fa
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-15 17:13:43 +0100

    create template store fixes

--- src/modules/template-list/pages/template-list-page/TemplateListPage.tsx
+++ src/modules/template-list/pages/template-list-page/TemplateListPage.tsx
@@ -6,7 +6,7 @@ import {
 import { useEffect } from 'react';
 import { templatesConst } from '../../../../const/template.const';
 import { Template } from '../../../../model/template.model';
-import { Box, Card, Wrap, WrapItem } from '@chakra-ui/react';
+import { Wrap, WrapItem } from '@chakra-ui/react';
 
 export function TemplateListPage() {
   const dispatch = useDispatch();
@@ -17,20 +17,15 @@ export function TemplateListPage() {
   }, []);
 
   return (
-    <Wrap
-      m={'20px'}
-      spacing={'20px'}
-      mt={'20px'}
-      display={'flex'}
-      justifyContent={'space-evenly'}
-      alignItems={'center'}>
+    <Wrap m={'20px'} spacing={'20px'} mt={'20px'} justify={'space-evenly'}>
       {templates.map((el: Template) => {
         return (
           <WrapItem
             w={'354px'}
             h={'100px'}
             bgColor={'white'}
-            borderRadius={'20px'}>
+            borderRadius={'20px'}
+            key={el.id}>
             {el.name}
           </WrapItem>
         );
