e05e9923f22b66736cbcd5dba611e6e96e5ebd0b
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-22 18:34:28 +0100

    Add redirect to template preview page

--- src/modules/template-list/pages/template-list-page/TemplateListPage.tsx
+++ src/modules/template-list/pages/template-list-page/TemplateListPage.tsx
@@ -2,9 +2,11 @@ import { useSelector } from 'react-redux';
 import { selectTemplates } from '../../../../store/template/templates.slice';
 import { Template } from '../../../../model/template.model';
 import { Wrap, WrapItem } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 export function TemplateListPage() {
   const templates = useSelector(selectTemplates);
+  const navigate = useNavigate();
 
   return (
     <Wrap m={'20px'} spacing={'20px'} mt={'20px'} justify={'space-evenly'}>
@@ -15,7 +17,9 @@ export function TemplateListPage() {
             h={'100px'}
             bgColor={'white'}
             borderRadius={'20px'}
-            key={el.id}>
+            key={el.id}
+            onClick={() => navigate(el.id)}
+            cursor={'pointer'}>
             {el.name}
           </WrapItem>
         );
