afd30258a1996bd6ed62a6568bc8cecee16b6e43
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-26 19:25:54 +0100

    Add display list of resumes

---
+++ src/modules/resume-list/ResumeLayout.tsx
@@ -0,0 +1,31 @@
+import { useHttpClient } from '../../hooks/http-client/use-http-client';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectLoadedResumes,
+  setResumes,
+} from '../../store/resume/resume.slice';
+import { Resume } from '../../model/resume.model';
+import { Loader } from '../../components/loader/Loader';
+import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+
+export function ResumeLayout() {
+  const http = useHttpClient();
+  const isLoaded = useSelector(selectLoadedResumes);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLoaded) {
+      return;
+    }
+    http
+      .get<Resume[]>('resumes')
+      .then((resumes) => dispatch(setResumes(resumes)));
+  }, []);
+
+  if (!isLoaded) {
+    return <Loader></Loader>;
+  }
+
+  return <Outlet />;
+}
