3ea1002a81b48271f3a6b22b49e0570a69da1714
Ewelina <ewelina.petelicka01@gmail.com>
2024-03-28 19:23:00 +0100

    add template searching

--- src/modules/template-list/pages/template-list-page/TemplateListPage.tsx
+++ src/modules/template-list/pages/template-list-page/TemplateListPage.tsx
@@ -1,29 +1,56 @@
 import { useSelector } from 'react-redux';
-import { selectTemplates } from '../../../../store/template/templates.slice';
+import {
+  selectTemplates,
+  selectTemplatesByFilters,
+} from '../../../../store/template/templates.slice';
 import { Template } from '../../../../model/template.model';
-import { Wrap, WrapItem } from '@chakra-ui/react';
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import { SearchIcon } from '@chakra-ui/icons';
+import React, { useState } from 'react';
 
 export function TemplateListPage() {
-  const templates = useSelector(selectTemplates);
+  const [query, setQuery] = useState('');
+  const templates = useSelector(selectTemplatesByFilters(query));
   const navigate = useNavigate();
 
   return (
-    <Wrap m={'20px'} spacing={'20px'} mt={'20px'} justify={'space-evenly'}>
-      {templates.map((el: Template) => {
-        return (
-          <WrapItem
-            w={'354px'}
-            h={'100px'}
-            bgColor={'white'}
-            borderRadius={'20px'}
-            key={el.id}
-            onClick={() => navigate(el.id)}
-            cursor={'pointer'}>
-            {el.name}
-          </WrapItem>
-        );
-      })}
-    </Wrap>
+    <Flex direction={'column'} m={'20px'}>
+      <Flex justifyContent={'flex-end'}>
+        <InputGroup w={300}>
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            bg="white"
+            placeholder="Search..."
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </InputGroup>
+      </Flex>
+      <Wrap m={'20px'} spacing={'20px'} mt={'20px'} justify={'space-evenly'}>
+        {templates.map((el: Template) => {
+          return (
+            <WrapItem
+              w={'354px'}
+              h={'100px'}
+              bgColor={'white'}
+              borderRadius={'20px'}
+              key={el.id}
+              onClick={() => navigate(el.id)}
+              cursor={'pointer'}>
+              {el.name}
+            </WrapItem>
+          );
+        })}
+      </Wrap>
+    </Flex>
   );
 }
