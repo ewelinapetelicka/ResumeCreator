17b31e784b1ae200b116ea0e9a5297fa0057c941
Tomasz Pasternak <tomasz.pasternak@nexocode.com>
2024-05-12 11:12:13 +0200

    Integrate unit testing environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,9 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  redirect,
-  RouterProvider,
-} from 'react-router-dom';
-import { Layout } from './Layout';
-import { ResumeListPage } from './modules/resume-list/pages/resume-list-page/ResumeListPage';
-import { TemplateListPage } from './modules/templates/pages/template-list-page/TemplateListPage';
+import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { TemplatePreviewPage } from './modules/templates/pages/template-preview-page/TemplatePreviewPage';
-import { TemplateLayout } from './modules/templates/TemplateLayout';
 import { theme } from './theme';
-import { NotFoundPage } from './pages/not-found-page/NotFoundPage';
-import { ResumeLayout } from './modules/resume-list/ResumeLayout';
-import { AuthorizedRoute } from './components/authorized-route/AuthorizedRoute';
-import { LoginPage } from './pages/login-page/LoginPage';
-import { UnauthorizedPage } from './pages/unauthorized-page/UnauthorizedPage';
-import { RegisterPage } from './pages/register-page/RegisterPage';
-import { ProfilePage } from './modules/profile/pages/profile-page/ProfilePage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout></Layout>,
-    children: [
-      {
-        index: true,
-        loader: async () => redirect('/templates'),
-      },
-      {
-        path: 'profile',
-        element: (
-          <AuthorizedRoute>
-            <ProfilePage />
-          </AuthorizedRoute>
-        ),
-      },
-      {
-        path: 'resumes',
-        element: (
-          <AuthorizedRoute>
-            <ResumeLayout />
-          </AuthorizedRoute>
-        ),
-        children: [
-          {
-            path: '',
-            element: <ResumeListPage />,
-          },
-        ],
-      },
-      {
-        path: 'templates',
-        element: <TemplateLayout />,
-        children: [
-          {
-            path: '',
-            element: <TemplateListPage />,
-          },
-          {
-            path: ':id',
-            element: <TemplatePreviewPage />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: 'login',
-    element: <LoginPage />,
-  },
-  {
-    path: 'register',
-    element: <RegisterPage />,
-  },
-  {
-    path: '404',
-    element: <NotFoundPage />,
-  },
-  {
-    path: '401',
-    element: <UnauthorizedPage />,
-  },
-  {
-    path: '*',
-    element: <Navigate to={'/templates'}></Navigate>,
-  },
-]);
+import { router } from './router.tsx';
 
 export function App() {
   return (
